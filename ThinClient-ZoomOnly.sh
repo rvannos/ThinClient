2fb535cc0feb4a961b8a450bff874f55ce42da6c
rvannos <48300792+rvannos@users.noreply.github.com>
2024-05-08 14:32:00 -0400

    Update ThinClient-ZoomOnly.sh
    wrong client download

--- ThinClient-ZoomOnly.sh
+++ ThinClient-ZoomOnly.sh
@@ -8,7 +8,7 @@ sed -i '1,2 s/^##*//' /etc/apt/sources.list
 sudo apt-get update
 
 # Thin Client VDI Plugin
-wget https://zoom.us/download/vdi/5.17.11.24850/zoomvdi-universal-plugin-ubuntu_5.17.11.deb
+wget https://zoom.us/download/vdi/5.17.11.24850/zoomvdi-universal-plugin-thinpro_5.17.11.deb
 chmod 755 zoomvdi-universal-plugin-thinpro_5.17.11.deb
 dpkg -i zoomvdi-universal-plugin-thinpro_5.17.11.deb
 rm zoomvdi-universal-plugin-thinpro_5.17.11.deb
