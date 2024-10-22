bc3b3fe17db1dd6be000cb6b276dd8663707de06
rvannos <48300792+rvannos@users.noreply.github.com>
2024-10-22 07:32:50 -0400

    Update ThinClient-ZoomOnly.sh
    updated to 6.1.12

--- ThinClient-ZoomOnly.sh
+++ ThinClient-ZoomOnly.sh
@@ -8,7 +8,7 @@ sed -i '1,2 s/^##*//' /etc/apt/sources.list
 sudo apt-get update
 
 # Thin Client VDI Plugin
-wget -O zoom_plugin.deb https://zoom.us/download/vdi/6.1.10.25260/zoomvdi-universal-plugin-thinpro_6.1.10.deb
+wget -O zoom_plugin.deb https://zoom.us/download/vdi/6.1.12.25370/zoomvdi-universal-plugin-thinpro_6.1.12.deb
 chmod 755 zoom_plugin.deb
 dpkg -i zoom_plugin.deb
 rm zoom_plugin.deb
